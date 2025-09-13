525bd64b4ee24cedccae529f5fdd28a1c63150ff
Naimadx123 <damianx787@gmail.com>
2025-09-13 15:37:01 +0200

    feat: add PackRelocListener to handle post-pack generation events and implement resource pack copying logic

--- src/main/kotlin/zone/vao/nexoPackfileReloc/NexoPackfileReloc.kt
+++ src/main/kotlin/zone/vao/nexoPackfileReloc/NexoPackfileReloc.kt
@@ -1,14 +1,47 @@
 package zone.vao.nexoPackfileReloc
 
 import org.bukkit.plugin.java.JavaPlugin
+import java.io.File
+import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.StandardCopyOption
 
 class NexoPackfileReloc : JavaPlugin() {
 
     override fun onEnable() {
-        // Plugin startup logic
+        saveDefaultConfig()
+        server.pluginManager.registerEvents(PackRelocListener(this), this)
     }
 
     override fun onDisable() {
         // Plugin shutdown logic
+    }
+
+    fun copyPackZip(copyToPath: String) {
+        val serverRoot = File(server.getPluginManager().getPlugin("Nexo")!!.dataFolder, "pack")
+
+        val source = File(serverRoot, "pack.zip")
+
+        val normalizedDestPath = copyToPath.removePrefix("/")
+        val dest = File(serverRoot, normalizedDestPath)
+
+        if (!source.exists()) {
+            throw IOException("Source file not found: ${source.absolutePath}")
+        }
+        if (!source.isFile) {
+            throw IOException("Source is not a file: ${source.absolutePath}")
+        }
+
+        dest.parentFile?.let { parent ->
+            if (!parent.exists() && !parent.mkdirs()) {
+                throw IOException("Unable to create destination directory: ${parent.absolutePath}")
+            }
+        }
+
+        Files.copy(
+            source.toPath(),
+            dest.toPath(),
+            StandardCopyOption.REPLACE_EXISTING
+        )
     }
 }
--- src/main/kotlin/zone/vao/nexoPackfileReloc/PackRelocListener.kt
+++ src/main/kotlin/zone/vao/nexoPackfileReloc/PackRelocListener.kt
@@ -1,4 +1,22 @@
 package zone.vao.nexoPackfileReloc
 
-class PackRelocListener {
+import com.nexomc.nexo.api.events.resourcepack.NexoPostPackGenerateEvent
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+
+class PackRelocListener(private val plugin: NexoPackfileReloc) : Listener {
+
+    @EventHandler
+    fun onPackGenerate(event: NexoPostPackGenerateEvent) {
+
+        val copyTo = plugin.config.getString("copy_to_path", "/generated/pack.zip")!!
+
+        runCatching { plugin.copyPackZip(copyTo) }
+            .onSuccess {
+                plugin.logger.info("Pack file copied -> $copyTo")
+            }
+            .onFailure {
+                plugin.logger.warning("Pack file copy failed : ${it.message}")
+            }
+    }
 }
